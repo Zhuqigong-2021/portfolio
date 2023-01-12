409f93b7c2adfdc5d9f6482cc2678c639631ea96
Zhuqigong-2021 <1406846879@qq.com>
2023-01-11 20:45:41 -0500

    back

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,12 @@
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   // define: {
-//   //   'process.env.SOME_ENV': `"${process.env.SOME_ENV}"`,
-//   //   // 'process.env.REACT_APP_SANITY_PROJECT_ID': `"${process.env.REACT_APP_SANITY_PROJECT_ID}"`,
-//   //   // 'process.env.REACT_APP_SANITY_TOKEN': `"${process.env.REACT_APP_SANITY_TOKEN}"`,
-//   // },
-// });
-
 import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-// import svgr from '@svgr/rollup';
+import react from '@vitejs/plugin-react';
 
-// export default defineConfig({
-//   plugins: [svgr(), react()],
-// });
-import reactRefresh from '@vitejs/plugin-react-refresh';
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()],
-  mode: 'development',
-  build: {
-    minify: false,
+  plugins: [react()],
+  define: {
+    'process.env.SOME_ENV': `"${process.env.SOME_ENV}"`,
+    // 'process.env.REACT_APP_SANITY_PROJECT_ID': `"${process.env.REACT_APP_SANITY_PROJECT_ID}"`,
+    // 'process.env.REACT_APP_SANITY_TOKEN': `"${process.env.REACT_APP_SANITY_TOKEN}"`,
   },
 });
