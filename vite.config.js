2dd90111f8b3fd9155f75a15859a3945697f2248
Zhuqigong-2021 <1406846879@qq.com>
2023-01-11 19:56:39 -0500

    change vite config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,20 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   // define: {
+//   //   'process.env.SOME_ENV': `"${process.env.SOME_ENV}"`,
+//   //   // 'process.env.REACT_APP_SANITY_PROJECT_ID': `"${process.env.REACT_APP_SANITY_PROJECT_ID}"`,
+//   //   // 'process.env.REACT_APP_SANITY_TOKEN': `"${process.env.REACT_APP_SANITY_TOKEN}"`,
+//   // },
+// });
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import svgr from '@svgr/rollup';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  define: {
-    'process.env.SOME_ENV': `"${process.env.SOME_ENV}"`,
-    // 'process.env.REACT_APP_SANITY_PROJECT_ID': `"${process.env.REACT_APP_SANITY_PROJECT_ID}"`,
-    // 'process.env.REACT_APP_SANITY_TOKEN': `"${process.env.REACT_APP_SANITY_TOKEN}"`,
-  },
+  plugins: [svgr(), react()],
 });
